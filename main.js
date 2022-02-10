d423f60ce60b42236a621ac35a4b250c0c3cea9c
jynadal <nadaljeanyves@hotmail.fr>
2022-02-10 16:31:20 +0100

    slider adding and BF form

--- main.js
+++ main.js
@@ -7,6 +7,8 @@
 //       console.log("Registration Failed: " + error);
 //     });
 
+const { createLog } = require("gatsby-cli/lib/reporter/redux/actions")
+
 //   navigator.seviceWorker
 //     .register("sw.js")
 //     .then((registration) => {
@@ -18,3 +20,26 @@
 //     });
 // } else {
 // }
+
+
+// let image_slider = document.getElementByClassName('image_slider');
+// console.log(image_slider)
+
+// let etape = 0;
+
+// let nbr_img = image_slider.length;
+
+// let precedent = document.querySelector('.precedent');
+// let suivant = document.querySelector('.suivant')
+
+// function enleverActiveImages() {
+// for(let i=0; i<nbr_img;  i++) {
+//     image_slider[i].classList.remove('active');
+//     }
+// }
+
+// suivant.addEventListener('click', function() {
+//     etape++;
+//     enleverActiveImages();
+//     image_slider[etape].classList.add('active');
+// })
